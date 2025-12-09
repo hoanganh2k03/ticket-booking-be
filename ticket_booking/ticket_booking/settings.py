918f6dd3ddde2e5514c7011d7621cd372b1eff78
hoanganh2k03 <n21dccn101@student.ptithcm.edu.vn>
2025-12-09 16:16:26 +0700

    1

--- ticket_booking/ticket_booking/settings.py
+++ ticket_booking/ticket_booking/settings.py
@@ -48,6 +48,10 @@ EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 
+
+
+
+
 EMAIL_HOST_USER = 'vuquochoanganh2k3@gmail.com'
 EMAIL_HOST_PASSWORD = 'tgai qoii itcl oroc'
 GROQ_API_KEY='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'

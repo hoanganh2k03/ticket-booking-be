791ad0874d064350a01bd0482d8599d22509cdc0
hoanganh2k03 <n21dccn101@student.ptithcm.edu.vn>
2025-12-09 17:42:06 +0700

    1

--- ticket_booking/ticket_booking/settings.py
+++ ticket_booking/ticket_booking/settings.py
@@ -47,8 +47,9 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'hattoriheiji48691810@gmail.com'
-EMAIL_HOST_PASSWORD = 'yunn cvuv glju xebd'
+EMAIL_HOST_USER = 'vuquochoanganh2k3@gmail.com'
+EMAIL_HOST_PASSWORD = 'tgai qoii itcl oroc'
+
 GROQ_API_KEY='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'
 # calery
 from celery.schedules import crontab  # Thêm import ở đây
